7e8e261a254a66c78e52f1741b174ae201aee786
Henrik Andersson <h@hnra.dev>
2023-12-16 19:38:32 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -13,7 +13,41 @@ vim.g.mapleader = ","
 
 -- Language Server Protocol Configs
 vim.cmd.packadd("nvim-lspconfig")
-require"lspconfig".hls.setup{}
+vim.o.updatetime = 1000
+local on_attach = function(_, bufnr)
+  vim.api.nvim_create_autocmd("CursorHold", {
+    buffer = bufnr,
+    callback = function()
+      local opts = {
+        focusable = false,
+        close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+        border = 'rounded',
+        source = 'always',
+        prefix = ' ',
+        scope = 'cursor',
+      }
+      vim.diagnostic.open_float(nil, opts)
+    end
+  })
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+end
+require"lspconfig".hls.setup{
+  on_attach = on_attach,
+}
+
+-- Remember last file position
+vim.api.nvim_create_autocmd('BufReadPost', {
+  pattern = "*",
+  -- group = vim.g.user.event,
+  callback = function(args)
+    local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
+    local not_commit = vim.b[args.buf].filetype ~= 'commit'
+
+    if valid_line and not_commit then
+      vim.cmd([[normal! g`"]])
+    end
+  end,
+})
 
 -- Fuzzy finder
 vim.cmd.packadd("plenary.nvim")
@@ -31,3 +65,4 @@ vim.cmd.packadd("FTerm.nvim")
 vim.keymap.set("n", "<leader>t", function()
   require("FTerm").toggle()
 end)
+vim.api.nvim_set_keymap('t', '<ESC>', '<C-\\><C-n>',  { noremap = true })
